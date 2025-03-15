ee858878c026bb1945c92081d61fa26ec4042c6a
nerdingout <thomas@estatespace.com>
2025-03-15 15:53:08 -0700

    set up user id delete aedit and articles view

--- handlers/PostArticle.go
+++ handlers/PostArticle.go
@@ -12,6 +12,7 @@ import (
 func PostArticle(ctx *fiber.Ctx) error {
 
 	mongoDB := ctx.Locals("mongoDB").(*mongo.Database)
+	userId := ctx.Locals("userId").(string)
 	timeNow := time.Now().UTC().Format(time.RFC3339)
 
 	if mongoDB == nil {
@@ -20,14 +21,21 @@ func PostArticle(ctx *fiber.Ctx) error {
 		})
 	}
 
+	userIdAsHex, err := primitive.ObjectIDFromHex(userId)
+
+	if err != nil {
+		return fiber.ErrInternalServerError
+	}
+
 	var articleCollection = mongoDB.Collection("Articles")
 
 	article := new(types.Article)
 
 	article.UpdatedAt = timeNow
 	article.CreatedAt = timeNow
+	article.CreatorId = userIdAsHex
 
-	err := ctx.BodyParser(&article)
+	err = ctx.BodyParser(&article)
 
 	if err != nil {
 		return ctx.Status(500).JSON(fiber.Map{
