fe4f6a26133c2b89981dfb9cada9b9690e82e120
nerdingout <thomas@estatespace.com>
2025-03-19 19:50:17 -0700

    fix: clean up logic

--- cmd/CreateCategories.go
+++ cmd/CreateCategories.go
@@ -25,19 +25,23 @@ func SeedCategoriesIfRequired() {
 
 	categoryCollection := client.Database("nerdingout").Collection("Categories")
 
-	var categories = make([]string, 5)
+	var categories = make([]string, 6)
 	categories[0] = "General"
 	categories[1] = "Skateboarding"
 	categories[2] = "Travel"
 	categories[3] = "Programming"
 	categories[4] = "Food / Beverage"
+	categories[5] = "Technology"
 
+	var count = 0
 	for _, category := range categories {
 		filter := bson.D{{"label", category}}
 		var cat = new(Category)
 		err = categoryCollection.FindOne(context.Background(), filter).Decode(&cat)
 
 		if errors.Is(err, mongo.ErrNoDocuments) {
+			count++
+
 			_, err := categoryCollection.InsertOne(context.Background(), bson.M{
 				"label": category,
 			})
@@ -45,8 +49,9 @@ func SeedCategoriesIfRequired() {
 			if err != nil {
 				fmt.Println("Cannot add")
 			}
-		} else {
-			fmt.Println("Item already exists", category)
 		}
 	}
+
+	fmt.Printf("Seeded %d categories\n", count)
+	fmt.Printf("%d categories unchanged\n", len(categories)-count)
 }
